1925a6b878308b9e7fa347160db798140d820983
Paul Jervis <jervis@12os.co.uk>
2025-02-12 19:27:14 +0000

    parse the number
      |
      |

---
+++ henry_and_paul/build.gradle.kts
@@ -0,0 +1,22 @@
+plugins {
+    kotlin("jvm") version "2.0.21"
+}
+
+group = "org.yorkdevelopers"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(kotlin("test"))
+    testImplementation("org.junit.jupiter:junit-jupiter:5.8.1")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
+kotlin {
+    jvmToolchain(19)
+}
